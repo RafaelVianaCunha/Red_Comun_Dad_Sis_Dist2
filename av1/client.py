24641528b9d65ef80c3b001e8a728bd56fe3f17b
Luis Carlos Gomes <lgomes@braspag.com.br>
2018-03-01 00:03:02 -0300

    Retorando palvra com acertos e erros

--- av1/client.py
+++ av1/client.py
@@ -7,10 +7,15 @@ porta_server=12397
 socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 #socket_cliente.bind((IP,porta))
-MSG="to enviando!"
-socket_cliente.sendto(MSG.encode('ascii'), (IP_server,porta_server))
 
-rec=socket_cliente.recvfrom(1024)
-print(rec)
+palpite = input('Qual o seu palpite: ')
 
-socket_cliente.close()
+while (palpite != 'sair'):
+    socket_cliente.sendto(palpite.encode('utf-8'), (IP_server, porta_server))
+
+    rec, addr = socket_cliente.recvfrom(1024)
+    print(rec.decode('utf-8'))
+
+    palpite = input('Qual o seu palpite: ')
+
+socket_cliente.close()  
--- av1/server.py
+++ av1/server.py
@@ -1,35 +1,43 @@
 import socket
-
-class jogador:
-    def __init__(self,nome):
-        _nome = nome
-    
-    
-class Jogo:
-    def __init__(self):
-        novoJogo(self)
-
-    def novoJogo(self):
-        self._file = open("palavras.txt", r) 
-        self.palavras =  file.readlines()
-        
-    def validarPalavra(palavra):
-        self._file = open("palavras.txt", r) 
-        self.palavras =  file.readlines()
-
+import random
 
 #servidor
 IP='localhost'
 porta=12397
 
+arquivo = open('palavras.txt', 'r') 
+palavras =  arquivo.readlines()
+
 socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 socket_servidor.bind((IP,porta))
 
-mgs, addr= socket_servidor.recvfrom(1024)
-print (mgs,addr)
+palavra = random.choices(palavras)[0].lower().replace('\n', '')
 
-socket_servidor.sendto(b'recebido',addr)
+erros = 0
 
+print(palavra)
+
+palpites = []
+
+while (True):
+    palpite, addr = socket_servidor.recvfrom(1024)
+    palpite = palpite.decode('utf-8').lower()
+    palpites.append(palpite)
+
+    print (palpite)
+
+    if (palpite not in palavra):
+        erros = erros + 1
+    
+    retorno = ''
+
+    for letra in palavra:
+        if (letra in palpites):
+            retorno = retorno + letra + ' '
+        else:
+            retorno = retorno + ' _ '
+
+    socket_servidor.sendto(retorno.encode('utf-8'), addr)
 
 socket_servidor.close()
