fa23ff8bab18eaedbeffc8c3122992243ccbc43c
Rafael Viana Cunha <rafaelvianacunha@outlook.com>
2018-03-07 22:32:09 -0300

    Update server.py

--- av1/server.py
+++ av1/server.py
@@ -1,43 +1,63 @@
-import socket
-import random
+import socket , pickle , random
+
+class Resposta:
+    def __init__(self, palavra, erros):
+        self.palavra = palavra
+        self.erros = erros
 
 #servidor
 IP='localhost'
 porta=12397
 
-arquivo = open('palavras.txt', 'r') 
-palavras =  arquivo.readlines()
 
-socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-socket_servidor.bind((IP,porta))
+class Jogo:
+    def __init__(self):
+        arquivo = open('palavras.txt', 'r') 
+        self.palavras =  arquivo.readlines()
+        self.Tot_erro = 7
+        self.erros = 0
+    def novoJogo(self):
+        print("Novo Jogo")
+        self.socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket_servidor.bind((IP,porta))
+        self.palavra = random.choices(self.palavras)[0].lower().replace('\n', '')
+        self.erros = 0
+        print(self.palavra)
+        self.palpites = []
+        self.main()
 
-palavra = random.choices(palavras)[0].lower().replace('\n', '')
+    def main(self):
+        while (self.erros<=(self.Tot_erro-1)):
+            palpite, addr = self.socket_servidor.recvfrom(1024)
+            palpite = palpite.decode('utf-8').lower()
+            self.palpites.append(palpite)
 
-erros = 0
+            print (palpite)
+           
+            if (palpite not in self.palavra):
+                self.erros = self.erros + 1
+            
+            retorno = ''
 
-print(palavra)
+            for letra in self.palavra:
+                if (letra in self.palpites):
+                    retorno = retorno + letra + ' '
+                else:
+                    retorno = retorno + ' _ '
+            
+            reposta = Resposta(retorno, self.erros)
+            data_string = pickle.dumps(reposta)
+            self.socket_servidor.sendto(data_string, addr)
+            if ('_' not in retorno):
+                print('Ganhou')
+                return
 
-palpites = []
+        self.novoJogo()
 
-while (True):
-    palpite, addr = socket_servidor.recvfrom(1024)
-    palpite = palpite.decode('utf-8').lower()
-    palpites.append(palpite)
+    def fimDeJogo(self):
+        self.socket_servidor.close()
 
-    print (palpite)
-
-    if (palpite not in palavra):
-        erros = erros + 1
-    
-    retorno = ''
-
-    for letra in palavra:
-        if (letra in palpites):
-            retorno = retorno + letra + ' '
-        else:
-            retorno = retorno + ' _ '
-
-    socket_servidor.sendto(retorno.encode('utf-8'), addr)
-
-socket_servidor.close()
+jogo = Jogo()
+jogo.novoJogo()
+jogo.fimDeJogo()
